af0118bf03c893bb04d8329d06ed05e5a0a6b6c5
Samantha Mashaal <Smash@Samanthas-MacBook-Pro-2.local>
2016-06-07 13:59:19 +0200

    Day7 morning

---
+++ Day7/Day7morning.js
@@ -0,0 +1,22 @@
+// window.onload = function() {
+	// console.log the values of First Name and Last Name 
+	function getFormValues( ) {  
+		// var first = document.getElementById("fname").value; 
+		// var last = document.getElementById("lname").value;
+		// console.log(first + last)
+
+		// var x = document.getElementById("form1").value;
+		// for (i = 0; i < form1.length; i++) { 
+  //   	text += form1[i];
+		// }
+
+  		var firstName = document.getElementById("fname").value;
+  		console.log(firstName)
+  		var lastName = document.getElementById("lname").value;
+    	console.log(lastName); 
+} 
+
+	// Change the color of the div with id "color-div"
+	function changeColor() {
+	document.getElementById("color").style.color = "#cc00ff";
+	}
